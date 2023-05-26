29133e9a5e944cbd008bfb4cd7ff334b3d94c220
Pablo Hennique <phennique@gmail.com>
2023-05-26 12:05:07 -0400

    updated root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root to: 'lists#index'
   resources :movies, only: [:index]
 
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
